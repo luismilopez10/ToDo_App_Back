e9a2ba7c71c3d8334a056a77953cc1d0b99ea7c0
Luis Miguel López <luis.miguel.lopez.10@gmail.com>
2022-05-04 12:42:33 -0500

    Initial commit

---
+++ src/main/java/com/todo/todoappback/TodoAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todoappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todo/todoappback/TodoAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todoappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
